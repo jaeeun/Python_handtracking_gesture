d2188bd7799bfec00446fa828a11d2af080d1f05
jaeeun Yang <yangtkboy@gmail.com>
2022-11-21 10:49:58 +0900

    add hand tracking code

--- ui.py
+++ ui.py
@@ -1,13 +1,45 @@
+
+import csv
+import copy
+import argparse
+import itertools
+
 import PySimpleGUI as sg
-# import PySimpleGUIQt
 import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+from model import KeyPointClassifier
 
 
 def main():
-    video = "C:\\Users\\jaeeun84.yang\\Videos\\ARGlass\\2020-05-27 19-53-13.mp4"
-    vidFile = cv.VideoCapture(0)
+    args = get_args()
+    mode = args.mode
+    number = args.number
+
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    hands = mp_hands.Hands(
+        static_image_mode='store_true',
+        max_num_hands=2,
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.5,
+    )
+
+    keypoint_classifier = KeyPointClassifier()
+
+    with open('model/keypoint_classifier/keypoint_classifier_label.csv',
+              encoding='utf-8-sig') as f:
+        keypoint_classifier_labels = csv.reader(f)
+        keypoint_classifier_labels = [
+            row[0] for row in keypoint_classifier_labels
+        ]
+
+
+
+    cap = cv.VideoCapture(0)
     # ---===--- Get some Stats --- #
-    fps = vidFile.get(cv.CAP_PROP_FPS)
+    fps = cap.get(cv.CAP_PROP_FPS)
 
     sg.theme('Black')
 
@@ -79,11 +111,12 @@ def main():
 
     # ---===--- LOOP through video file by frame --- #
     cur_frame = 0
-    while vidFile.isOpened():
+    while cap.isOpened():
+        
         event, values = window.read(timeout=timeout)
         if event in ('Exit', None):
             break
-        ret, frame = vidFile.read()
+        ret, image = cap.read()
         if not ret:  # if out of data stop looping
             break
         # if someone moved the slider manually, the jump to that frame
@@ -93,10 +126,46 @@ def main():
         # slider_v.update(cur_frame)
         # cur_frame += 1
 
-        imgbytes = cv.imencode('.ppm', frame)[1].tobytes()  # can also use png.  ppm found to be more efficient
+        
+        
+
+
+        image = cv.flip(image, 1)  # ミラー表示
+        debug_image = copy.deepcopy(image)
+
+        # 검출 #############################################################
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+
+        if results.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
+                                                  results.multi_handedness):                                  
+                mp_drawing.draw_landmarks(debug_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+                # calc_bounding_rect
+                brect = calc_bounding_rect(debug_image, hand_landmarks)
+                # 랜드마크
+                landmark_list, landmark_3Dlist = calc_landmark_list(debug_image, hand_landmarks)
+
+                pre_processed_landmark_list = pre_process_landmark(landmark_list)
+                # sign 분류
+                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+
+                
+                debug_image = draw_info_text(
+                            debug_image,
+                            brect,
+                            handedness,
+                            keypoint_classifier_labels[hand_sign_id],
+                        )
+
+        debug_image = draw_info(debug_image, fps, mode, number)
+
+        imgbytes = cv.imencode('.ppm', debug_image)[1].tobytes()  # can also use png.  ppm found to be more efficient
         image_elem.update(data=imgbytes)
-        
-        
 
         if event is 'Move':
             m+=1
@@ -130,5 +199,104 @@ def main():
             h = int(values['-SLIDER_V-']) * 4
             graph.MoveFigure(line_h,0, h-svH)
             svH=h
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=int, default=0)
+    parser.add_argument("--number", type=int, default=0)
+    args = parser.parse_args()
+
+    return args
+
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+    landmark_3dpoint = []
+
+    # 키 포인트
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        landmark_z = landmark.z * image_height
+
+        landmark_point.append([landmark_x, landmark_y])
+        landmark_3dpoint.append([landmark_x, landmark_y, landmark_z])
+
+    return landmark_point,landmark_3dpoint
+
+
+def draw_info(image, fps, mode, number):
+    cv.putText(image, "FPS:" + str(fps), (10, 30), cv.FONT_HERSHEY_SIMPLEX,
+               0.8, (0, 0, 0), 4, cv.LINE_AA)
+    cv.putText(image, "FPS:" + str(fps), (10, 30), cv.FONT_HERSHEY_SIMPLEX,
+               0.8, (255, 255, 255), 2, cv.LINE_AA)
+
+    mode_string = ['Logging Key Point', 'Logging Point History']
+    if 0 <= mode <= 2:
+        cv.putText(image, "MODE:" + mode_string[mode - 1], (10, 90),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv.LINE_AA)
+        if 0 <= number <= 9:
+            cv.putText(image, "NUM:" + str(number), (10, 110),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                       cv.LINE_AA)
+    return image
+
+def draw_info_text(image, brect, handedness, hand_sign_text):
+    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
+                 (0, 0, 0), -1)
+
+    info_text = handedness.classification[0].label[0:]
+    if hand_sign_text != "":
+        info_text = info_text + ':' + hand_sign_text
+    cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
+               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
+
+    return image
+
+def pre_process_landmark(landmark_list):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    # 상대 좌표로 변환
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+    # 1次元リストに変換
+    temp_landmark_list = list(
+        itertools.chain.from_iterable(temp_landmark_list))
+
+    # 正規化
+    max_value = max(list(map(abs, temp_landmark_list)))
+
+    def normalize_(n):
+        return n / max_value
+
+    temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+    return temp_landmark_list
 
 main()
